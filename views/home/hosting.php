d25a813b1612734890ae0cef31ac37526b14dc07
mark <mark@lussumo.com>
2009-08-01 16:41:19 -0600

    gimme a break

--- views/home/hosting.php
+++ views/home/hosting.php
@@ -7,16 +7,24 @@
 <div id="Content">
    <div class="container_12 clearfix">
       <h1>Looking for a great web host?</h1>
-      <p>There are a bajillion hosting companies out there on the interweb, and finding the right place to host your Vanilla forum can be tough. Listed below are some of our recommended hosting partners that not only meet the Vanilla minimum requirements, but offer great service and support.</p>
-      
-      <p>If you do end up choosing one of the hosting companies listed on this page, you'll get all the benefits of working with a great hosting company, and they will donate a portion of your payment back to Vanilla and will help to keep Vanilla alive and well.</p>
-
-      <p>If you don't need the flexibility of a full web host, you should <a href="http://vanillaforums.com">consider signing up for a forum at VanillaForums.com</a>.</p>
-
-      <h2>Host X</h2>
-      <p>About Host X</p>
-      
-      <h2>Host Y</h2>
-      <p>About Host Y</p>
+      <div class="grid_6">
+         <p>There are a bajillion hosting companies out there on the interweb, and finding the right place to host your Vanilla forum can be tough. Listed below are some of our recommended hosting partners that not only meet the Vanilla minimum requirements, but offer great service and support.</p>
+         
+         <p>If you do end up choosing one of the hosting companies listed on this page, you'll get all the benefits of working with a great hosting company, and they will donate a portion of your payment back to Vanilla and will help to keep Vanilla alive and well.</p>
+   
+         <p>If you don't need the flexibility of a full web host, you should <a href="http://vanillaforums.com">consider signing up for a forum at VanillaForums.com</a>.</p>
+      </div>
+      <div class="grid_6">
+         <div class="Host Featured">
+            <h4><a href="http://www.rackspacecloud.com">Rackspace Cloud</a></h4>
+            <p>
+               <a href="http://www.rackspacecloud.com"><img alt="Cloud Hosting and Cloud Computing by Rackspace - Formerly Mosso" src="<?php echo Asset('/applications/vanillaforumsorg/design/the-rackspace-cloud-dark-250-wide.png'); ?>" border="0"/></a>
+               This is where we host all of our websites and services. We've found the folks at Rackspace to be fast, efficient, and extremely helpful. You really can't beat Rackspace when it comes to rock-steady hosting & fanatical support. Prices start at $11 / month with cloud servers.
+            </p>
+            <p style="color: #d00;">If you choose to sign up with Rackspace, be sure to enter our referral code: <strong>PTNR-VANILLA</strong></p>
+         </div>
+         <div class="Host">
+         </div>
+      </div>
    </div>
 </div>
