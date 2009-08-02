e1d43b22ecbeeb6db406b1f8436a29b326ae0dbd
mark <mark@lussumo.com>
2009-08-02 17:04:55 -0600

    asdf

--- js/get.js
+++ js/get.js
@@ -2,7 +2,9 @@ jQuery(document).ready(function($) {
    var loc = location.href;
    if (loc.substring(0,-1) != '/')
       loc += '/';
-   
-   loc += '1';
-   document.location = loc;
+
+   if (loc.substring(0, -5) != '/1/1/') {
+      loc += '1';
+      document.location = loc;
+   }
 });
