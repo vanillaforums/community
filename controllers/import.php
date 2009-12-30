59e55e58a5b1d36291892331b308d7caaae79134
mark <mark@vanillaforums.com>
2009-12-30 12:14:01 -0600

    Changed licensing info. Fixed css & js references.

--- controllers/import.php
+++ controllers/import.php
@@ -1,11 +1,11 @@
 <?php if (!defined('APPLICATION')) exit();
 /*
-Copyright 2008, 2009 Mark O'Sullivan
+Copyright 2008, 2009 Vanilla Forums Inc.
 This file is part of Garden.
 Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
-Contact Mark O'Sullivan at mark [at] lussumo [dot] com
+Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
 */
 
 /**
@@ -23,8 +23,7 @@ class ImportController extends VFOrgController {
    public function Index($Step = 0) {
       $this->Permission('Garden.Data.Import'); // This permission doesn't exist, so only users with Admin == '1' will succeed.
       
-      if ($this->Head)
-         $this->Head->AddScript('/applications/vanilla/js/import.js');
+      $this->AddJsFile('import.js', 'vanilla');
       
       $Step = is_numeric($Step) && $Step >= 0 && $Step < 30 ? $Step : '';
       $Database = Gdn::Database();
@@ -505,8 +504,7 @@ class ImportController extends VFOrgController {
    public function OldIndex($Step = 0) {
       $this->Permission('Garden.Data.Import'); // This permission doesn't exist, so only users with Admin == '1' will succeed.
       
-      if ($this->Head)
-         $this->Head->AddScript('/applications/vanilla/js/import.js');
+      $this->AddJsFile('import.js', 'vanilla');
       
       $Step = is_numeric($Step) && $Step >= 0 && $Step < 30 ? $Step : '';
       $Database = Gdn::Database();
@@ -986,14 +984,13 @@ class ImportController extends VFOrgController {
    
    public function Initialize() {
       $this->Head = new HeadModule($this);
-      $this->Head->AddScript('js/library/jquery.js');
-      $this->Head->AddScript('js/library/jquery.livequery.js');
-      $this->Head->AddScript('js/library/jquery.form.js');
-      $this->Head->AddScript('js/library/jquery.popup.js');
-      $this->Head->AddScript('js/library/jquery.menu.js');
-      $this->Head->AddScript('js/library/jquery.gardenhandleajaxform.js');
-      $this->Head->AddScript('js/global.js');
-
+      $this->AddJsFile('js/library/jquery.js');
+      $this->AddJsFile('js/library/jquery.livequery.js');
+      $this->AddJsFile('js/library/jquery.form.js');
+      $this->AddJsFile('js/library/jquery.popup.js');
+      $this->AddJsFile('js/library/jquery.menu.js');
+      $this->AddJsFile('js/library/jquery.gardenhandleajaxform.js');
+      $this->AddJsFile('js/global.js');
       $this->AddCssFile('setup.css');
       Gdn_Controller::Initialize();
    }   
