d13d087249a238ba541a8c618ff543ecdb09c754
mark <mark@vanillaforums.com>
2010-03-29 17:22:14 -0400

    text

--- views/home/splash.php
+++ views/home/splash.php
@@ -6,23 +6,21 @@
       <!--
       <h3>Vanilla is an open-source, standards-compliant, international, customizable, simple, free discussion forum for the web.</h3>
       -->
-      <div class="Download">
-         <p>If you are an IT manager, a developer, or a do-it-yourselfer and you have web space, technical know-how, and gumption, click here for the free downloadable version of Vanilla Forums.</p>
-         <?php
-            echo Anchor(
-               Img('/themes/vanillaforumsorg/design/icons/down_48.png')
-               ."You do the work",
-               '/vforg/home/index'
-            );
-         ?>
-      </div>
       <div class="Host">
-         <p>If you are a business owner, brand manager, or a non- technical person and you want your community up and running in just a few moments hassle-free, click here for free Vanilla Forum hosting.</p>
          <?php
             echo Anchor(
                Img('/themes/vanillaforumsorg/design/icons/promotion_new_48.png')
-               ."We do the work",
+               ."If you are a business owner, brand manager, or a non- technical person and you want your community up and running in just a few moments hassle-free, click here for free Vanilla Forum hosting.",
                'http://vanillaforums.com'
+            );
+         ?>
+      </div>
+      <div class="Download">
+         <?php
+            echo Anchor(
+               Img('/themes/vanillaforumsorg/design/icons/down_48.png')
+               ."If you are an IT manager, a developer, or a do-it-yourselfer and want to install Vanilla Forums in your own environtment, click here for the open source download.",
+               '/vforg/home/index'
             );
          ?>
       </div>
