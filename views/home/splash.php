ac12daea9d6e077ef651ab728b4b2e675812dbc7
mark <mark@vanillaforums.com>
2010-03-29 17:05:38 -0400

    text changes

--- views/home/splash.php
+++ views/home/splash.php
@@ -4,19 +4,25 @@
       <h1>The <em>sweetest</em> forum on the web.</h1>
       <h2>Over 300,000 people use Vanilla to build community around their website, brand, or business. For free.</h2>
       <h3>Vanilla is an open-source, standards-compliant, international, customizable, simple, free discussion forum for the web.</h3>
-      <?php
-         echo Anchor(
-            Img('/themes/vanillaforumsorg/design/icons/down.png')
-            ."If you are an IT manager, a developer, or a do-it-yourselfer and you have web space, technical know-how, and gumption, click here for the free downloadable version of Vanilla Forums.",
-            '/home/index',
-            'Download'
-         );
-         echo Anchor(
-            Img('/themes/vanillaforumsorg/design/icons/promotion_new.png')
-            ."If you are a business owner, brand manager, or a non- technical person and you want your community up and running in just a few moments hassle-free, click here for free Vanilla Forum hosting.",
-            'http://vanillaforums.com',
-            'Host'
-         );
-      ?>
+      <div class="Download">
+         <p>If you are an IT manager, a developer, or a do-it-yourselfer and you have web space, technical know-how, and gumption, click here for the free downloadable version of Vanilla Forums.</p>
+         <?php
+            echo Anchor(
+               Img('/themes/vanillaforumsorg/design/icons/down_48.png')
+               ."You do the work",
+               '/vforg/home/index'
+            );
+         ?>
+      </div>
+      <div class="Host">
+         <p>If you are a business owner, brand manager, or a non- technical person and you want your community up and running in just a few moments hassle-free, click here for free Vanilla Forum hosting.</p>
+         <?php
+            echo Anchor(
+               Img('/themes/vanillaforumsorg/design/icons/promotion_new_48.png')
+               ."We do the work",
+               'http://vanillaforums.com'
+            );
+         ?>
+      </div>
    </div>
 </div>
