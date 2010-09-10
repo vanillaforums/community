c00b03b9864d861fcf19a984f82b0e172e981688
Mark O'Sullivan <mark@vanillaforums.com>
2010-09-10 17:03:09 -0400

    typo

--- views/updates/rows.php
+++ views/updates/rows.php
@@ -6,7 +6,7 @@ foreach ($this->UpdateData->Result('Text') as $Update) {
    <tr<?php echo $Alt ? ' class="Alt"' : ''; ?>>
       <td><?php echo Anchor($Update->Location, $Update->Location); ?></td>
       <td class="Alt"><?php echo $Update->RemoteIp; ?></td>
-      <td><?php echo number_format($Update->CountComments; ?></td>
+      <td><?php echo number_format($Update->CountComments); ?></td>
       <td class="Alt"><?php echo number_format($Update->CountDiscussions); ?></td>
       <td><?php echo number_format($Update->CountUsers); ?></td>
       <td class="Alt"><?php echo Gdn_Format::Date($Update->DateInserted); ?></td>
