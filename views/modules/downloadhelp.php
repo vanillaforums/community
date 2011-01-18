9f3b383f472722c674f568608e1f90d754b4e018
Todd Burry <todd@vanillaforums.com>
2011-01-18 18:36:31 -0500

    Changed the download/stable link to get/vanilla-core.

--- views/modules/downloadhelp.php
+++ views/modules/downloadhelp.php
@@ -2,7 +2,7 @@
 <div class="Box DownloadHelp">
    <h4>Available Downloads</h4>
    <ul class="SideMenu">
-      <li><strong><?php echo Anchor('Latest Stable Release', '/download/get/stable'); ?> &rarr;</strong></li>
+      <li><strong><?php echo Anchor('Latest Stable Release', '/get/vanilla-core'); ?> &rarr;</strong></li>
       <li><?php echo Anchor('Release Archive', '/page/ReleaseArchive'); ?></li>
    </ul>
 </div>
