119e7e597d57498cf337b25ca9d98e782349dadc
Mark O'Sullivan <mark@vanillaforums.com>
2011-01-20 17:20:54 -0500

    Fixed up extended feed format.

--- views/home/getfeed.php
+++ views/home/getfeed.php
@@ -5,15 +5,16 @@ echo '<div class="Feed">';
 foreach ($this->Feed->channel->item as $Item) {
    if ($Loop > $this->MaxLength)
       break;
-   
+
    $Title = GetValue('title', $Item);
-   $Excerpt = GetValue('excerpt', $Item);
    $Link = GetValue('link', $Item);
-   $PubDate = GetValue('pubDate', $Item);
+   $PubDate = strtotime(GetValue('pubDate', $Item));
    if ($this->FeedFormat == 'extended') {
+      $Description = GetValue('description', $Item);
       echo Wrap(
          Anchor($Title, $Link)
-         .$Excerpt,
+         .Wrap(Gdn_Format::Date($PubDate), 'div', array('class' => 'Date'))
+         .Wrap($Description, 'em'),
          'div',
          array('class' => 'FeedItem')
       );
