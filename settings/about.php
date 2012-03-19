688ee28b9e133c0c3c95a4df0f4d3ad83bd6616a
Mark O'Sullivan <mark@vanillaforums.com>
2012-03-19 15:23:54 -0400

    Version udpate

--- settings/about.php
+++ settings/about.php
@@ -3,7 +3,7 @@
 $ApplicationInfo['VFOrg'] = array(
    'Name' => 'VanillaForums.org',
    'Description' => "The VanillaForums.org website.",
-   'Version' => '1.1',
+   'Version' => '1.2',
    'RegisterPermissions' => FALSE,
    'SetupController' => 'setup',
    'Author' => "Mark O'Sullivan",
