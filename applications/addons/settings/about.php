8b45f09ae30b14940f4ebed2511b84e1f084ab76
kasperisager <kasperisager@gmail.com>
2014-03-04 11:09:29 -0500

    Buuuuuuump!

--- applications/addons/settings/about.php
+++ applications/addons/settings/about.php
@@ -3,7 +3,7 @@
 $ApplicationInfo['Addons'] = array(
    'Name' => 'Vanilla Forums Addons',
    'Description' => "The addon management tool for VanillaForums.org.",
-   'Version' => '1.0.3',
+   'Version' => '1.0.4',
    'RegisterPermissions' => FALSE,
    'SetupController' => 'setup',
    'Author' => "Mark O'Sullivan",
