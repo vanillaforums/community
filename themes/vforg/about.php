d90ab00c524e0214c0f422de2b0c33ba22e864c0
Todd Burry <todd@vanillaforums.com>
2014-10-28 12:08:13 -0400

    Update the vforg theme.
    - Resize the addon icons in the list.
    - Remove some duplicated css.

--- themes/vforg/about.php
+++ themes/vforg/about.php
@@ -3,7 +3,7 @@
 $ThemeInfo['VanillaForums.org'] = array(
    'Name' => 'VanillaForums.org',
    'Description' => "A custom theme for VanillaForums.org. Used to customize the heading banner for the forum.",
-   'Version' => '1.5.1',
+   'Version' => '1.5.2',
    'Author' => "Mark O'Sullivan",
    'AuthorEmail' => 'mark@vanillaforums.com',
    'AuthorUrl' => 'http://markosullivan.ca',
