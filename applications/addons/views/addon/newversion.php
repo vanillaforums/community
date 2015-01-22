d01a2b4d207734b036b1fe122fd065565502834f
lincolnwebs <lincolnwebs@gmail.com>
2015-01-22 10:11:04 -0500

    Require GPL2-compatible addons

--- applications/addons/views/addon/newversion.php
+++ applications/addons/views/addon/newversion.php
@@ -5,7 +5,12 @@ if ($this->DeliveryType() == DELIVERY_TYPE_ALL)
 
 ?>
 <h2><?php echo T('Upload a New Version'); ?></h2>
-<div class="Info"><?php echo T('By uploading a file you certify that you have the right to distribute this addon and that it does not violate the Terms of Service.'); ?></div>
+<div class="Info"><?php
+	echo T('Addons allowable license info', "All addons must declare an appropriate GPL2-compatible license.
+   These include: GNU GPL2, MIT, BSD, GNU LGPL, or Mozilla Public License (MPL) v2.
+   Addons uploaded without explicit license information will be declared as GNU GPL2.
+   By uploading your new or revised addon, you agree to these terms.");
+?></div>
 <?php
    echo $this->Form->Open(array('enctype' => 'multipart/form-data'));
    echo $this->Form->Errors();
@@ -13,7 +18,9 @@ if ($this->DeliveryType() == DELIVERY_TYPE_ALL)
 <ul>
 	<li>
 		<h3><?php echo $this->Form->Label('File to Upload (2mb max)', 'File'); ?></h3>
-		<?php echo $this->Form->Input('File', 'file', array('class' => 'File')); ?>
+		<?php echo $this->Form->Input('File', 'file', array('class' => 'File'));
+		echo '<div class="Info">', T('By uploading a file you certify that you have the right to distribute this addon and that it does not violate the Terms of Service.'), '</div>';
+		?>
 	</li>
 	<!--<li>
 		<?php
