54985c067b19219ea496a08294858f780936d5bf
Francis Bolap <francis.b@vanillaforums.com>
2015-09-01 11:56:23 -0400

    VFOrg theme: Version bump

--- applications/addons/controllers/class.getcontroller.php
+++ applications/addons/controllers/class.getcontroller.php
@@ -16,7 +16,7 @@ class GetController extends AddonsController {
     public $Uses = array('Form', 'Database', 'AddonModel');
 
     public function Index($ID = '', $ServeFile = '0') {
-        $this->AddJsFile('jquery.js');
+        $this->AddJsFile('js/library/jquery.js');
 
         // Define the item being downloaded
         if (strtolower($ID) == 'vanilla')
