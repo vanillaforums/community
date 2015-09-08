3b287f63304a6257f186a59cc2b0853305299b18
lincolnwebs <lincolnwebs@gmail.com>
2015-09-08 16:45:47 -0400

    Revert "Add margins to feature line items"
    This reverts commit 3170c44ef3581124d5a2be3fc7674e31c2d584a0.

--- applications/vforg/settings/about.php
+++ applications/vforg/settings/about.php
@@ -3,7 +3,7 @@
 $ApplicationInfo['VFOrg'] = array(
     'Name' => 'VanillaForums.org',
     'Description' => "The VanillaForums.org website.",
-    'Version' => '1.4.1',
+    'Version' => '1.4',
     'Author' => "Vanilla Community",
     'AuthorEmail' => 'support@vanillaforums.com',
     'AuthorUrl' => 'http://vanillaforums.org',
