62017f2f67c2ffcfc7df05d50abb9182db3caadb
lincolnwebs <lincolnwebs@gmail.com>
2015-12-25 12:09:03 -0500

    Change Addons app name

--- applications/addons/settings/about.php
+++ applications/addons/settings/about.php
@@ -1,10 +1,9 @@
 <?php
 
 $ApplicationInfo['Addons'] = array(
-    'Name' => 'Vanilla Forums Addons',
+    'Name' => 'Addons Directory',
     'Description' => "The addon management tool for VanillaForums.org.",
     'Version' => '1.1',
-    'SetupController' => 'setup',
     'Author' => "Vanilla Community",
     'AuthorEmail' => 'support@vanillaforums.com',
     'AuthorUrl' => 'http://vanillaforums.org',
