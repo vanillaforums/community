f4886c741c6e32e3354efec4aa34dc1942e8fdae
lincolnwebs <lincolnwebs@gmail.com>
2015-12-25 12:09:27 -0500

    Do some code cleanup

--- applications/addons/settings/bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php if (!defined('APPLICATION')) exit();
-
-require_once(PATH_APPLICATIONS . DS . 'addons' . DS . 'library' . DS . 'functions.render.php');
--- applications/addons/settings/class.hooks.php
+++ applications/addons/settings/class.hooks.php
@@ -235,3 +235,24 @@ class AddonsHooks implements Gdn_IPlugin {
         SaveToConfig('Addons.Version', $Version);
     }
 }
+
+if (!function_exists('RenderDiscussionAddonWarning')) {
+function RenderDiscussionAddonWarning($AddonID, $AddonName, $AttachID) {
+  $DeleteOption = '';
+  if (Gdn::Session()->CheckPermission('Addons.Addon.Manage')) {
+    $DeleteOption = Anchor(
+                    'x',
+                    'addon/detachfromdiscussion/' . $AttachID,
+                    array('class' => 'Dismiss'));
+  }
+  $String = Wrap(
+          $DeleteOption .
+          sprintf(
+                  T('This discussion is related to the %s addon.'), Anchor(
+                          $AddonName, 'addon/' . $AddonID . '/' . Gdn_Format::Url($AddonName)
+                  )
+          ), 'div', array('class' => 'Warning AddonAttachment DismissMessage')
+  );
+  return $String;
+}
+}
