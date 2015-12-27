fc280f072d7d753a118b7a996833c9129eee5801
lincolnwebs <lincolnwebs@gmail.com>
2015-12-27 11:51:56 -0500

    Addon directory 1.2

--- applications/addons/settings/about.php
+++ applications/addons/settings/about.php
@@ -3,7 +3,7 @@
 $ApplicationInfo['Addons'] = array(
     'Name' => 'Addons Directory',
     'Description' => "The addon management tool for VanillaForums.org.",
-    'Version' => '1.1',
+    'Version' => '1.2',
     'Author' => "Vanilla Community",
     'AuthorEmail' => 'support@vanillaforums.com',
     'AuthorUrl' => 'http://vanillaforums.org',
