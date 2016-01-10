17970e6db374de113a1c66a4f64e41372e71420c
lincolnwebs <lincolnwebs@gmail.com>
2016-01-09 19:00:15 -0500

    Cache break theme

--- themes/vforg/about.php
+++ themes/vforg/about.php
@@ -3,7 +3,7 @@
 $ThemeInfo['VanillaForums.org'] = array(
    'Name' => 'VanillaForums.org',
    'Description' => "A custom theme for VanillaForums.org. Used to customize the heading banner for the forum.",
-   'Version' => '1.6.4',
+   'Version' => '1.6.5',
    'Author' => "Mark O'Sullivan",
    'AuthorEmail' => 'mark@vanillaforums.com',
    'AuthorUrl' => 'http://markosullivan.ca',
