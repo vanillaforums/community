a02add50b08cdd7699340e40c99fc644f1fef694
lincolnwebs <lincolnwebs@gmail.com>
2016-07-03 11:35:29 -0400

    Remove dupe HomeController and fix some standards

--- applications/vforg/settings/class.hooks.php
+++ applications/vforg/settings/class.hooks.php
@@ -10,32 +10,116 @@
 /**
  * Class VFOrgHooks
  */
-class VFOrgHooks implements Gdn_IPlugin {
+class VFOrgHooks extends Gdn_Plugin {
 
     /**
      * Remove mobile rendering from our custom non-forum pages.
      *
-     * @param $Sender
+     * @param $sender
      */
-    public function base_render_before($Sender) {
-        $userFacing = $Sender->MasterView == 'default' || $Sender->MasterView == '';
-        $onForumPage = !in_array(strtolower($Sender->Application), array('vanilla', 'conversations', 'dashboard'));
+    public function base_render_before($sender) {
+        $userFacing = $sender->MasterView == 'default' || $sender->MasterView == '';
+        $onForumPage = !in_array(strtolower($sender->Application), array('vanilla', 'conversations', 'dashboard'));
         if ($userFacing && isMobile() && $onForumPage) {
             // Use the main theme instead of mobile
-            $Sender->Theme = c('Garden.Theme');
-            Gdn::pluginManager()->unRegisterPlugin('MobileThemeHooks');
+            $sender->Theme = c('Garden.Theme');
+            Gdn::pluginManager()->unregisterPlugin('MobileThemeHooks');
         }
     }
 
     /**
      * Add menu items to Dashboard.
      *
-     * @param $Sender
+     * @param $sender
      */
-    public function base_getAppSettingsMenuItems_handler($Sender) {
-        $Menu = $Sender->EventArguments['SideMenu'];
+    public function base_getAppSettingsMenuItems_handler($sender) {
+        $Menu = $sender->EventArguments['SideMenu'];
         $Menu->addLink('Site Settings', 'Update Checkers', 'updates/', 'Garden.Settings.Manage');
         $Menu->addLink('Site Settings', 'Download Summary', 'vstats', 'Garden.Settings.Manage');
+    }
+
+    /**
+     * Homepage of VanillaForums.org.
+     *
+     * @param Gdn_Controller $sender
+     */
+    public function homeController_homepage_create($sender) {
+        try {
+            $AddonModel = new AddonModel();
+            $Addon = $AddonModel->getSlug('vanilla-core', true);
+            $sender->setData('CountDownloads', val('CountDownloads', $Addon));
+            $sender->setData('Version', val('Version', $Addon));
+            $sender->setData('DateUploaded', val('DateInserted', $Addon));
+        } catch (Exception $ex) {
+        }
+        $sender->title('The most powerful custom community solution in the world');
+        $sender->setData('Description', "Vanilla is forum software that powers discussions on hundreds of thousands of sites. Built for flexibility and integration, Vanilla is the best, most powerful community solution in the world.");
+        $sender->Head->addTag('meta', array('name' => 'description', 'content' => $sender->data('Description')));
+
+        $sender->clearJsFiles();
+        $sender->addJsFile('jquery.js');
+        $sender->addJsFile('easySlider1.7.js');
+        saveToConfig('Garden.Embed.Allow', false, false); // Prevent JS errors
+
+        $sender->clearCssFiles();
+        $sender->addCssFile('vforg-home.css');
+        $sender->MasterView = 'empty';
+        $sender->render('index');
+    }
+
+    /**
+     * Woe unto those who wander here; abandon all logic and hope.
+     *
+     * @param Gdn_Controller $sender
+     * @param string $Type
+     * @param int $Length
+     * @param string $FeedFormat
+     */
+    public function homeController_getFeed_create($sender, $Type = 'news', $Length = 5, $FeedFormat = 'normal') {
+        $sender->MaxLength = is_numeric($Length) && $Length <= 50 ? $Length : 5;
+        $sender->FeedFormat = $FeedFormat;
+        switch ($Type) {
+            case 'releases':
+            case 'help':
+                // Once you realize we're consuming our own local RSS you can never unsee it.
+                $Url = 'http://vanillaforums.org/categories/blog/feed.rss';
+                break;
+            case 'news':
+            case 'cloud':
+            default:
+                $Url = 'http://blog.vanillaforums.com/feed/';
+        }
+
+        $RawFeed = file_get_contents($Url);
+        $sender->Feed = new SimpleXmlElement($RawFeed);
+        $sender->render('getfeed', 'home', 'vforg');
+    }
+
+    /**
+     * Proxy an RSS feed for Dashboard use across our kingdom.
+     *
+     * @param Gdn_Controller $sender
+     * @param $Url
+     * @return mixed|string
+     * @throws Exception
+     */
+    public function homeController_proxyFeed_create($sender, $Url) {
+        $Key = 'Feed|'.$Url;
+        $Feed = Gdn::cache()->get($Key);
+        if (!$Feed) {
+            $Feed = ProxyRequest($Url, 5);
+            Gdn::cache()->store($Key, $Feed, array(Gdn_Cache::FEATURE_EXPIRY => 5 * 60));
+        }
+        return $Feed;
+    }
+
+    /**
+     * Splish splash I was takin' a... modal ad for cloud?
+     *
+     * @param Gdn_Controller $sender
+     */
+    public function homeController_splash_create($sender) {
+        $sender->render();
     }
 
     /**
@@ -43,8 +127,6 @@ class VFOrgHooks implements Gdn_IPlugin {
      */
     public function setup() {
         $this->structure();
-
-
     }
 
     /**
--- applications/vforg/views/home/getfeed.php
+++ applications/vforg/views/home/getfeed.php
@@ -3,28 +3,31 @@
 $Loop = 1;
 echo '<div class="Feed">';
 foreach ($this->Feed->channel->item as $Item) {
-    if ($Loop > $this->MaxLength)
+    if ($Loop > $this->MaxLength) {
         break;
+    }
 
-    $Title = GetValue('title', $Item);
-    $Link = GetValue('link', $Item);
-    $PubDate = strtotime(GetValue('pubDate', $Item));
+    $Title = val('title', $Item);
+    $Link = val('link', $Item);
+    $PubDate = strtotime(val('pubDate', $Item));
     if ($this->FeedFormat == 'extended') {
-        $Description = GetValue('description', $Item);
+        $Description = val('description', $Item);
         // Cut off after the first subheading.
         $Description = array_shift(explode('<h', $Description));
-        echo Wrap(
-            Wrap(Anchor($Title, $Link), 'h2')
-                .Wrap(Gdn_Format::Date($PubDate), 'div', array('class' => 'Date'))
-                .Wrap($Description, 'div', array('class' => 'FeedDescription')),
-            'div', array('class' => 'FeedItem ExtendedFormat')
+        echo wrap(
+            wrap(anchor($Title, $Link), 'h2')
+                .wrap(Gdn_Format::date($PubDate), 'div', ['class' => 'Date'])
+                .wrap($Description, 'div', ['class' => 'FeedDescription']),
+            'div',
+            ['class' => 'FeedItem ExtendedFormat']
         );
     } else {
-        echo Wrap(
+        echo wrap(
             '<span class="Sprite SpriteRarr SpriteRarrDown">&rarr;</span>'
-                .Anchor($Title, $Link)
-                .Wrap(Gdn_Format::Date($PubDate), 'div', array('class' => 'Date')),
-            'div', array('class' => 'FeedItem NormalFormat')
+                .anchor($Title, $Link)
+                .wrap(Gdn_Format::date($PubDate), 'div',['class' => 'Date']),
+            'div',
+            ['class' => 'FeedItem NormalFormat']
         );
     }
     $Loop++;
