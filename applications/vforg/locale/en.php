31a5e0855899c78a7bb4bdeeebca72db62f4f2a0
Lincoln Russell <lincoln@icrontic.com>
2017-12-23 10:38:56 -0500

    Code purge

--- applications/vforg/locale/en.php
+++
@@ -1,4 +0,0 @@
-<?php if (!defined('APPLICATION')) exit();
-
-$Definition['TestedWith'] = 'Testing information';
-$Definition['AddonTypeID'] = 'Addon type';
--- applications/vforg/views/modules/downloadhelp.php
+++
@@ -1,8 +0,0 @@
-<?php if (!defined('APPLICATION')) exit(); ?>
-<div class="Box DownloadHelp">
-    <h4>Available Downloads</h4>
-    <ul class="SideMenu">
-        <li><strong><?php echo Anchor('Latest Stable Release', '/get/vanilla-core'); ?> &rarr;</strong></li>
-        <li><?php echo Anchor('Release Archive', '/page/ReleaseArchive'); ?></li>
-    </ul>
-</div>
