1b76641edf057382b2991ae9b0ac1db3d58273ae
Adam Charron <adam@charrondev.com>
2019-03-02 14:43:54 -0500

    Fix addon help panel classes

--- applications/addons/views/modules/addonhelp.php
+++ applications/addons/views/modules/addonhelp.php
@@ -8,23 +8,25 @@ if (Gdn::session()->isValid()) {
 }
 ?>
 
-<div class="Box">
+<div class="Box BoxFilter">
     <h4>Do it yourself!</h4>
-    <ul>
-        <?php echo wrap(anchor(t('How to Contribute'), 'http://docs.vanillaforums.com/developers/contributing/'), 'li'); ?>
-        <?php echo wrap(anchor(t('Plugin Quickstart'), 'http://docs.vanillaforums.com/developers/plugins/quickstart'), 'li'); ?>
-        <?php echo wrap(anchor(t('Vanilla On GitHub'), 'https://github.com/vanilla/vanilla'), 'li'); ?>
-        <?php echo wrap(anchor(t('This Directory&rsquo;s Code'), 'https://github.com/vanilla/community'), 'li'); ?>
+    <ul class="PanelInfo">
+        <li><a class="ItemLink" href="http://docs.vanillaforums.com/developers/contributing/">How to Contribute</a></li>
+        <li><a class="ItemLink" href="http://docs.vanillaforums.com/developers/plugins/quickstart">Plugin Quickstart</a></li>
+        <li><a class="ItemLink" href="https://github.com/vanilla/vanilla">Vanilla On GitHub</a></li>
+        <li><a class="ItemLink" href="https://github.com/vanilla/community">This Directory's Code</a></li>
     </ul>
 </div>
 
-<div class="Box What">
+<div class="Box">
     <h4>What is this stuff?</h4>
-    <p>Addons are custom features that you can add to your Vanilla forum. Addons are created by our community of developers and people like you!</p>
+    <p class="PanelInfo">Addons are custom features that you can add to your Vanilla forum. Addons are created by our community of developers and people like you!</p>
 </div>
 
-<div class="Box Work">
+<div class="Box">
     <h4>Will it work on my Vanilla Forum?</h4>
-    <p>These addons are for people who downloaded and set up their own Vanilla forum. Compare your downloaded version with the version requirements on the right.</p>
-    <p>If your Vanilla forum is hosted at <a href="http://vanillaforums.com">VanillaForums.com</a>, this addon may already be installed there.</p>
+    <div class="PanelText userContent">
+        <p>These addons are for people who downloaded and set up their own Vanilla forum. Compare your downloaded version with the version requirements on the right.</p>
+        <p>If your Vanilla forum is hosted at <a href="http://vanillaforums.com">VanillaForums.com</a>, this addon may already be installed there.</p>
+    </div>
 </div>
