3369c60f6be7002b491befff436b8cbf8bf991b6
Todd Burry <todd@vanillaforums.com>
2020-04-16 13:02:15 -0400

    Fix the call to addLink() when the menu is null
    The controller’s menu can be null if we are in an API context or a partial render context. Most hooks do an `is_object()` check or a plain truthy check.

--- applications/addons/settings/class.hooks.php
+++ applications/addons/settings/class.hooks.php
@@ -19,7 +19,9 @@ class AddonsHooks implements Gdn_IPlugin {
      * @param Gdn_Controller $sender Sender object.
      */
     public function base_render_before(Gdn_Controller $sender) {
-        $sender->Menu->addLink('Addons', t('Addons'), '/addon/browse/all/recent', false, ['class' => 'Addons']);
+        if (is_object($sender->Menu)) {
+            $sender->Menu->addLink('Addons', t('Addons'), '/addon/browse/all/recent', false, ['class' => 'Addons']);
+        }
     }
 
     /**
